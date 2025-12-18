420ffaba5b0d432db08c4dd311331efd970d9155
PaulAlejoR <chicarcas_par@hotmail.com>
2025-12-18 15:42:39 -0600

    Archivo centralizado para la info de la pagina de categorias

---
+++ src/data/categoriasData.ts
@@ -0,0 +1,52 @@
+/* Archivo para centralizar los datos de categorías, así se evita tener un array en un y otro objeto en otro, para evitar que se rompa si algo se cambia de alguno de los archivos */
+
+export const categoriasData = [
+    {
+        id: 1,
+        slug: 'noticias',
+        name: 'Noticias',
+        description: 'El noticiero que te mantendrá informado con las noticias más importantes. Conduce Karla Pulido, una voz confiable que te llevará la información de manera clara y oportuna.',
+    },
+    {
+        id: 2,
+        slug: 'deportes',
+        name: 'Deportes',
+        description: 'Tu fuente de noticias y analisis deportivo. Con entrevistas exclusivas, reportajes especiales y la cobertura de tus deportes favoritos, conectamos la pasión por el deporte con los fans más exigentes. ',
+    },
+    {
+        id: 3,
+        slug: 'entretenimiento',
+        name: 'Entretenimiento',
+        description: 'Sumérgete en el mundo del espectáculo con nuestro programa de entretenimiento. Desde las últimas noticias de celebridades hasta reseñas de películas y música, te mantenemos al tanto de todo lo que sucede en la industria del entretenimiento.',
+    },
+    {
+        id: 4,
+        slug: 'musicales',
+        name: 'Musicales',
+        description: 'Sumérgete en el mundo de la música con nuestro programa musical. Desde las últimas novedades y tendencias hasta entrevistas exclusivas con artistas destacados, te mantenemos al día con todo lo que sucede en el universo musical.',
+    },
+    {
+        id: 5,
+        slug: 'espectaculos',
+        name: 'Espectaculos',
+        description: 'Sumérgete en el mundo del espectáculo con nuestro programa de entretenimiento. Desde las últimas noticias de celebridades hasta reseñas de películas y música, te mantenemos al tanto de todo lo que sucede en la industria del entretenimiento.',
+    },
+    {
+        id: 6,
+        slug: 'ciencia-y-cultura',
+        name: 'Ciencia y Cultura',
+        description: 'Explora la riqueza de las culturas del mundo con nuestro programa cultural. Desde tradiciones ancestrales hasta manifestaciones artísticas contemporáneas, te llevamos en un viaje fascinante a través de la diversidad cultural que nos rodea.',
+    },
+    {
+        id: 7,
+        slug: 'negocios',
+        name: 'Negocios',
+        description: 'Mantente informado sobre el mundo de los negocios con nuestro programa especializado. Desde análisis de mercado hasta entrevistas con líderes empresariales, te proporcionamos las herramientas y conocimientos necesarios para comprender el panorama económico actual.',
+    },
+    {
+        id: 8,
+        slug: 'salud',
+        name: 'Salud',
+        description: 'Mantente informado sobre el mundo de la salud con nuestro programa especializado. Desde consejos médicos hasta reportajes sobre avances científicos, te proporcionamos información actualizada y confiable para tu bienestar.',
+    }
+];
